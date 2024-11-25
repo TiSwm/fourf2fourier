252c47f049ec3d65341e8e8cc59ea7253f4e5eb1
y1tschwe, Tilman Schwemmer <tilman.schwemmer@zeiss.com>
2024-11-25 15:14:59 +0100

    bfix - flipping the first lens helps a lot

--- zoom_first_lens.py
+++ zoom_first_lens.py
@@ -13,16 +13,22 @@ def get_model(shift_obj=0, shift_tube=0, NA=0.04):
     osp['wvls'] = WvlSpec([(550.0, 1.0)], ref_wl=0)
     opm.radius_mode = True
 
-    sm.gaps[0].thi=28.17 + shift_obj
-    sm.add_surface([+20.89, 12.0, 'N-BAF10', 'Schott'])
-    sm.add_surface([-16.73, 2.00, 'N-SF6HT', 'Schott'])
-    sm.add_surface([-79.8, 22.9])
+    # sm.gaps[0].thi=28.17 + shift_obj
+    # sm.add_surface([+20.89, 12.0, 'N-BAF10', 'Schott'])
+    # sm.add_surface([-16.73, 2.00, 'N-SF6HT', 'Schott'])
+    # sm.add_surface([-79.8, 22.9])
+
+    sm.gaps[0].thi=21.175 + shift_obj
+    sm.add_surface([+79.8, 2.00, 'N-SF6HT', 'Schott'])
+    sm.add_surface([+16.73, 12.00, 'N-BAF10', 'Schott'])
+    sm.add_surface([-20.89, 22.9, ])
+
     sm.set_stop()
 
     sm.add_surface([np.inf, 150.0 - shift_obj + shift_tube])
     sm.add_surface([+91.62, 5.7, 'N-BK7', 'Schott'])
     sm.add_surface([-66.68, 2.2, 'SF5', 'Schott'])
-    sm.add_surface([-197.7, 172.07 - shift_tube])
+    sm.add_surface([-197.7, 172.071 - shift_tube])
 
     opm.update_model()
 
@@ -49,38 +55,51 @@ def calc_contrast(sm, pitch, x0=0, na=0.04):
     return 1 - center_int
 
 def mtf_guess(sm):
-    pitches = np.linspace(5, 200, 20) * 1e-3
+    pitches = np.linspace(5, 100, 30) * 1e-4
     contrast = np.zeros_like(pitches)
     for i, p in enumerate(pitches):
         contrast[i] = calc_contrast(sm, p, 0.6 / 5, 0.04)
-    return pitches, contrast
+    return 1e3 * pitches, contrast
 
 
-shifts = np.linspace(-1, 1, 20)
+shifts = np.linspace(-.1, .1, 60)
 contrast_at_50um = np.zeros_like(shifts)
 
 fig, ax = plt.subplots()
 for i, shift in enumerate(shifts):
     sm = get_model(shift)
-    contrast_at_50um[i] = calc_contrast(sm, 50e-3)
-    ax.plot(*mtf_guess(sm))
+    contrast_at_50um[i] = calc_contrast(sm, 1e-3)
+    if i % 6 == 0:
+        ax.plot(*mtf_guess(sm), label=f"shift 1st Lens: {np.around(shift * 1e3, 1)}um")
+ax.set_xlabel("structure pitch / um")
+ax.set_ylabel("ray optical contrast")
+ax.legend()
 plt.show()
 
 fig, ax = plt.subplots()
-ax.plot(shifts, contrast_at_50um)
+ax.plot(1e3 * shifts, contrast_at_50um)
+ax.set_xlabel("shift 1st Lens / um")
+ax.set_ylabel("ray optical contrast @ pitch=1um")
+ax.legend()
 plt.show()
 
 
-shifts = np.linspace(-20, 20, 20)
+shifts = np.linspace(-2, 2, 60)
 contrast_at_50um = np.zeros_like(shifts)
 
 fig, ax = plt.subplots()
 for i, shift in enumerate(shifts):
     sm = get_model(0., shift)
-    contrast_at_50um[i] = calc_contrast(sm, 50e-3)
-    ax.plot(*mtf_guess(sm))
+    contrast_at_50um[i] = calc_contrast(sm, 1e-3)
+    if i % 6 == 0:
+        ax.plot(*mtf_guess(sm), label=f"shift 2nd Lens: {np.around(shift, 1)}mm")
+ax.set_xlabel("structure pitch / um")
+ax.set_ylabel("ray optical contrast")
+ax.legend()
 plt.show()
 
 fig, ax = plt.subplots()
 ax.plot(shifts, contrast_at_50um)
+ax.set_xlabel("shift 2nd Lens / mm")
+ax.set_ylabel("ray optical contrast @ pitch=1um")
 plt.show()
